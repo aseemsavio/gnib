f6a6af0cb893be75091151063f4b5a5f2a144503
aseemsavio <aseemsavio3@gmail.com>
2022-01-15 20:58:35 +0530

    basic changes made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>gnib - Graal VM Native Image Builder</h3>
+        <button className='button button1'>Upload your JAR file</button>
+        <textarea placeholder='Enter your custom "native-image .." command if any' className='textarea'></textarea>
+        <small>Building Native Image...</small>
+        <div className='flexbox-container'>
+          <br></br>
+          <button className='button button1'>Download the Native Image</button>
+          <button className='button button1'>Download Logs Dump</button>
+        </div>
       </header>
     </div>
   );
